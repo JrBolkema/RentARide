4701cb8bb55887efaaadf4d647b01e0aba8f5a9a
Matthew Wise <mwise@students.ccu.edu>
2019-10-01 14:04:32 -0600

    Updated name of VehicleController

--- RentARide/Controllers/VehicleController.cs
+++ RentARide/Controllers/VehicleController.cs
@@ -9,7 +9,7 @@ namespace RentARide.Controllers
 {
 	[Route("Vehicle")]
 	[ApiController]
-	public class ValuesController : ControllerBase
+	public class VehicleController : ControllerBase
 	{
 		// GET vehicle
 		[HttpGet]

9957c22948ae4800b3e8f13e27ddab9aa56856b2
Carl <carl@Carls-MacBook-Pro.local>
2019-10-27 13:21:10 -0600

    Frontend with Data for cars

--- RentARide/my-app/src/app/home/home.component.ts
+++ RentARide/my-app/src/app/home/home.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { vehicles } from '../vehicles';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  vehilces = vehicles;
+  vehicleNames = [];
+  show = false;
 
-  constructor() { }
+  constructor() {
+    vehicles.forEach(car => {
+      this.vehicleNames.push({Name : car.make, Visible : false});
+    });
+  }
 
   ngOnInit() {
   }
 
+  public getCars(car) {
+    if(this.show==false) {
+      this.show = true;
+      this.vehicleNames[this.vehilces.indexOf(car)].Visible = true;
+    } else {
+      this.show = false;
+      this.vehicleNames[this.vehilces.indexOf(car)].Visible = false;
+    }
+  }
+
 }
+

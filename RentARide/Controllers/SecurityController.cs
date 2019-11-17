376312d6a74223a59c0f052e79bb9bd786a0fc5e
Jon Bolkema <jrbolkema@gmail.com>
2019-11-17 13:57:20 -0700

    connected angular to dummy code for API

---
+++ RentARide/Controllers/SecurityController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace RentARide.Controllers
+{
+    [Route("security")]
+    [ApiController]
+    public class SecurityController : ControllerBase
+    {
+		[HttpGet("login")]
+		public bool Get(string username, string password)
+		{
+			if (username == "jon" && password == "1234")
+			{
+				return true;
+			} else {
+				return false;
+					}
+		}
+	}
+}

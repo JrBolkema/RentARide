ba3ad9cd63bcac877b75180ca808934ba0b5689b
Jon Bolkema <jrbolkema@gmail.com>
2019-12-09 20:44:03 -0700

    added fleet management, not working bc CORS issue

--- RentARide/my-app/src/app/app-routing.module.ts
+++ RentARide/my-app/src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { XyzComponent } from "./xyz/xyz.component";
+import { FleetComponent } from './fleet/fleet.component';
 
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent },
-  {path: 'xyz', component: XyzComponent }
+  {path: 'xyz', component: XyzComponent },
+  {path: 'fleetMaintainance', component: FleetComponent }
+
 
 ];
 
